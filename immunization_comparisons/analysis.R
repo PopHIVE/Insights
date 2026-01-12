1f490cbdba2670a5c844e422b789714b30d88ad3
weinbergerlab <dweinber@gmail.com>
2026-01-12 13:09:31 -0500

    try alternate definition for denominator in epic

--- immunization_comparisons/analysis.R
+++ immunization_comparisons/analysis.R
@@ -15,23 +15,47 @@ a1 <- read_csv('./data/mmr_over_time.csv', skip=11) %>%
          pct_vax_epic = as.numeric(pct_vax_epic)
          )
 
+#alternative definiion, uses all patients with an encounter as thebase
+b1 <- read_csv('./data/raw/all_encounter_base.csv', skip=12) %>%
+  rename(age = "Age at Encounter in Years" ,
+         geography = "State of Residence",
+         year = "Year",
+         pct_vax_epic_alt = "...4"              ) %>%
+  tidyr::fill(.,age, geography, .direction='down') %>%
+  mutate(year = str_extract(year, "\\d{4}"),
+         pct_vax_epic_alt = gsub('%','', pct_vax_epic_alt),
+         pct_vax_epic_alt = as.numeric(pct_vax_epic_alt)
+  )
 
+c1 <- a1 %>% 
+  full_join(b1, by=c('age','geography','year'))
+  
+  
 schoolvax <- read_parquet("https://github.com/PopHIVE/Ingest/raw/refs/heads/main/data/bundle_childhood_immunizations/dist/overall_rates_by_source.parquet") %>%
   filter(vaccine=='MMR')
 
 nis <- read_parquet("https://github.com/PopHIVE/Ingest/raw/refs/heads/main/data/bundle_childhood_immunizations/dist/overall_rates_by_source.parquet") %>%
   filter(vaccine=="≥1 Dose MMR" & age=='24 Months')
 
-schoolvax_epic_compare <- a1 %>%
+schoolvax_epic_compare <- c1 %>%
   filter(age=="? 4 and < 5 Years") %>%
   left_join(schoolvax, by=c('year','geography'))
 
-nis_epic_compare <- a1 %>%
+nis_epic_compare <- c1 %>%
   filter(age=="? 1 and < 2 Years") %>%
   left_join(nis, by=c('year','geography'))
 
 p1 <- ggplot(schoolvax_epic_compare)+
   geom_point(aes(x=pct_vax_epic, y=value, color=year, group=geography))+
+  ylim(70,100)+
+  xlim(70,100)+
+  facet_wrap(~year) 
+p1
+plotly::ggplotly(p1)
+
+
+p1 <- ggplot(schoolvax_epic_compare)+
+  geom_point(aes(x=pct_vax_epic_alt, y=value, color=year, group=geography))+
   ylim(70,100)+
   xlim(70,100)+
   facet_wrap(~year) 
@@ -52,13 +76,28 @@ p2
 
 plotly::ggplotly(p2)
 
+p2<- nis_epic_compare %>%
+  filter(!is.na(value)) %>%
+  ggplot()+
+  geom_point(aes(x=pct_vax_epic_alt, y=value, color=year, group=geography))+
+  ylim(40,100)+
+  xlim(40,100) + 
+  facet_wrap(~year) +
+  geom_abline(intercept=0, slope=1)+
+  ylab('NIS')
+p2
+
+plotly::ggplotly(p2)
+
 p3<- nis_epic_compare %>%
   filter(!is.na(value)) %>%
   mutate(year=as.numeric(year)) %>%
   ggplot()+
   geom_line(aes(x=year, y=value))+
   geom_line(aes(x=year, y=pct_vax_epic), color='red')+
-  ylim(80,100)+
+  geom_line(aes(x=year, y=pct_vax_epic_alt), color='blue')+
+  
+    ylim(40,100)+
   geom_abline(intercept=0, slope=1)+
   facet_wrap(~geography, nrow=5)+
   ylab('Uptake') +
